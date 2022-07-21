52c972d5fe28fd650ed927d7e93f69005ad57c9a
Coder Scare <102672802+Coder-Scare@users.noreply.github.com>
2022-07-21 20:15:27 +0100

    First Commit

---
+++ Mixedmessages.js
@@ -0,0 +1,11 @@
+const firstMessage = "Life is ";
+
+const secondMessage =  () => {
+    const message = ["a series of", "made up of", "about enjoying"];
+    const secondMessageRand = message[Math.floor(Math.random() * 3)];
+    return secondMessageRand;
+}
+
+
+
+

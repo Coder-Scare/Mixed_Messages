4c8f0eebc110b87f03af7cee1c5dddd4a5911720
Coder Scare <102672802+Coder-Scare@users.noreply.github.com>
2022-07-22 19:03:59 +0100

    added index and improved js file

--- Mixedmessages.js
+++ Mixedmessages.js
@@ -1,23 +1,21 @@
-const firstMessage = "Life is";
+// Function to return random message
+// Made more responive to adding or removing elements
 
-const secondMessage =  () => {
-    const message = ["a series of", "made up of", "about enjoying"];
-    const secondMessageRand = message[Math.floor(Math.random() * 3)];
-    return secondMessageRand;
+const onButtonClick = () => {
+    const makeRandomSentence = () => {
+        const sentence = [];
+        const first = ["Life is", "Life will be", "The future is", "Your existence is"];
+        const second = ["a series of", "made up of", "about enjoying"];
+        const last = ["moments.", "good times.", "milestones.", "pushing through hardships.", "being in the moment.", "time spent with loved ones."];
+        sentence.push(first[Math.floor(Math.random() * first.length)]);
+        sentence.push(second[Math.floor(Math.random() * second.length)]);
+        sentence.push(last[Math.floor(Math.random() * last.length)]);
+        const quote = sentence.join(' ');
+        return quote;
+
+    };
+
+    document.querySelector('.inspirational-quote').innerHTML = makeRandomSentence();
 }
-
-const lastMessage =  () => {
-    const message = ["moments.", "good times.", "milestones.", "pushing through hardships.", "being in the moment.", "time spent with loved ones."];
-    const lastMessageRand = message[Math.floor(Math.random() * 6)];
-    return lastMessageRand;
-}
-
-const completeMessage =  (first, second, last) => {
-    message = "Inspirational Quote: " + first + " " + second + " " + last;
-    return message;
-}
-
-console.log(completeMessage(firstMessage, secondMessage(), lastMessage()))
-
 
 
